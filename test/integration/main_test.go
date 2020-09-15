52799ecc5569a5de546e0fec1c95807548e0c07f
atoato88 <atoato88@gmail.com>
2020-09-15 21:53:15 +0900

    Add integrate tests for using net/http module

--- test/integration/main_test.go
+++ test/integration/main_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	"os/exec"
 	"testing"
 )
 
 func TestInfo(t *testing.T) {
-	c := exec.Command("curl", "http://localhost:8080/info")
+	c := exec.Command("curl", "--silent", "http://localhost:8080/info")
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	err := c.Run()
@@ -17,7 +18,7 @@ func TestInfo(t *testing.T) {
 }
 
 func TestHello(t *testing.T) {
-	c := exec.Command("curl", "http://localhost:8080/hello")
+	c := exec.Command("curl", "--silent", "http://localhost:8080/hello")
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	err := c.Run()
@@ -27,11 +28,41 @@ func TestHello(t *testing.T) {
 }
 
 func TestPing(t *testing.T) {
-	c := exec.Command("curl", "http://localhost:8080/ping")
+	c := exec.Command("curl", "--silent", "http://localhost:8080/ping")
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	err := c.Run()
 	if err != nil {
+		t.Error("error occurs when call /ping")
+	}
+}
+
+func TestInfo2(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/info")
+	if err != nil {
+		t.Error("error occurs when call /info")
+	}
+	if resp.StatusCode != 200 {
+		t.Error("status code isn't 200 when call /info")
+	}
+}
+
+func TestHello2(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/hello")
+	if err != nil {
 		t.Error("error occurs when call /hello")
+	}
+	if resp.StatusCode != 200 {
+		t.Error("status code isn't 200 when call /hello")
+	}
+}
+
+func TestPing2(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/ping")
+	if err != nil {
+		t.Error("error occurs when call /info")
+	}
+	if resp.StatusCode != 200 {
+		t.Error("status code isn't 200 when call /ping")
 	}
 }
